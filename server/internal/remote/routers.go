b150ea46e1cca69d85674529c46e0a49a0b85b8c
Quanzheng Long <qlong.seattle@gmail.com>
2026-07-17 15:37:24 -0700

    some minor cleanup

--- server/internal/remote/routers.go
+++ server/internal/remote/routers.go
@@ -5,5 +5,6 @@ import "github.com/superdurable/dex/protos/gengo/dexpb"
 type Routers interface {
 	ForRunsService(address string) (dexpb.RunsServiceClient, error)
 	ForTaskQueueService(address string) (dexpb.TaskQueueServiceClient, error)
+	// EvictCachedConn is for when a remote instance is shutdown, we need to evict the cached connection to it
 	EvictCachedConn(address string)
 }
