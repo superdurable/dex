460a6c5358b104807ef24c8eb5628457124a61bb
Quanzheng Long <prclqz@gmail.com>
2026-07-29 13:36:22 -0700

    Add step execution lineage tracking across SDK and server (#133)

--- server/service/common/rpc/invoke.go
+++ server/service/common/rpc/invoke.go
@@ -92,6 +92,10 @@ func InvokeWorkerRpc(
 	if err := validateWorkerRpcResponse(resp); err != nil {
 		return nil, err
 	}
+	service.SetFromStepExecutionID(
+		resp.GetStepDecision(),
+		service.GetFromStepExecutionIdForRPC(req.GetRpcName()),
+	)
 
 	if err := blobstore.OffloadLargeAttributeWrites(
 		ctx, resp.GetUpsertAttributes(), req.GetFlowId(),
