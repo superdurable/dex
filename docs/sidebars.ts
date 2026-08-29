3427797479a3c691cacc4bb6355d08ed709c1c79
Quanzheng Long <prclqz@gmail.com>
2026-08-28 22:55:42 -0700

    docs: persist sidebar category state (#396)

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -6,14 +6,12 @@ const sidebars: SidebarsConfig = {
     {
       type: 'category',
       label: 'Intro and Quick Start',
-      collapsed: false,
       items: ['intro/what-is-durable-execution', 'intro/what-is-dex', 'quick-start/index'],
     },
     {
       type: 'category',
       label: 'Primitives',
       link: {type: 'doc', id: 'primitives/index'},
-      collapsed: false,
       items: [
         {
           type: 'category',
