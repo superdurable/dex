a40e71592c71f3c8ad8456b01aaaaa9b3045919f
Quanzheng Long <qlong.seattle@gmail.com>
2026-08-31 17:05:35 -0700

    Normalize expired wait deadlines

--- sdk-go/dex/client.go
+++ sdk-go/dex/client.go
@@ -24,7 +24,9 @@ import (
 	"github.com/superdurable/dex/blob-cache-go/blobcache"
 	"github.com/superdurable/dex/sdk-go/gen/dexpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -1443,6 +1445,12 @@ func translateWaitRPCError(
 ) error {
 	if contextErr := ctx.Err(); contextErr != nil {
 		return contextErr
+	}
+	if status.Code(err) == codes.DeadlineExceeded {
+		deadline, hasDeadline := ctx.Deadline()
+		if hasDeadline && !time.Now().Before(deadline) {
+			return context.DeadlineExceeded
+		}
 	}
 	return translateRPCError(err, op, flowID, target)
 }
